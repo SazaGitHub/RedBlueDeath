74b9fc511d3d568861880a19733389d9ef0e33f1
SazaGitHub <51421046+SazaGitHub@users.noreply.github.com>
2020-09-14 23:43:56 -0400

    I made it
    yea

---
+++ index2.js
@@ -0,0 +1,22 @@
+function randomInteger(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const choices = ["red", "blue", "death"]; // death hurts
+// const choice = choices[randomInteger(0, 2)];
+
+function grab() {
+	return choices[randomInteger(0, 2)];
+}
+
+function multigrab(times) {
+	for (let i = 0; i < times; i++) {
+		console.log(grab());
+	}
+}
+
+while (true) {
+	if (prompt("e?") == "grab") {
+		alert(grab());
+	}
+}
